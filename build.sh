00516a16a77bf00b34c65aca2a77adc26dd55333
Yuri Pourre <yuripourre@gmail.com>
2017-11-13 17:08:25 -0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+mkdir -p libs
+cd libs
+wget http://libgdx.badlogicgames.com/nightlies/dist/gdx-natives.jar
+wget http://libgdx.badlogicgames.com/nightlies/dist/extensions/gdx-freetype/gdx-freetype-natives.jar
+cd ../libs
