922f9a1c9186d7621bce25c921581bad3001a09c
Yuri Pourre <yuripourre@gmail.com>
2017-12-06 23:05:53 -0200

    Use etyl-gdx-base-backend

--- src/main/java/com/harium/etyl/Etyl.java
+++ src/main/java/com/harium/etyl/Etyl.java
@@ -2,23 +2,29 @@ package com.harium.etyl;
 
 
 import com.harium.etyl.commons.context.Application;
+import com.harium.etyl.core.Core;
 import com.harium.etyl.core.Engine;
 import com.harium.etyl.loader.FontLoader;
 import com.harium.etyl.loader.MultimediaLoader;
 import com.harium.etyl.loader.image.ImageLoader;
+import com.harium.etyl.ui.GDXWindow;
 import com.harium.etyl.util.PathHelper;
 
-public abstract class Etyl extends DesktopEngine implements Engine<Application> {
+public abstract class Etyl extends DesktopEngine<Core> implements Engine<Application> {
 
     public static final String WINDOW = "window";
     private Application application;
+    protected Core core;
 
     public Etyl(int w, int h) {
         super(w, h);
     }
 
     public void init() {
+        core = initCore();
+
         initialSetup("");
+        core.getSession().put(Etyl.WINDOW, new GDXWindow());
 
         application = startApplication();
         core.setApplication(application);
