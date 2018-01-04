cec6a3dac55776c1f1235dd5a0a2c4a038c60c2f
Yuri Pourre <yuripourre@gmail.com>
2018-01-04 14:34:20 -0200

    Fix arc rendering

--- src/main/java/com/harium/etyl/core/graphics/GDXGraphics.java
+++ src/main/java/com/harium/etyl/core/graphics/GDXGraphics.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.utils.ScissorStack;
@@ -29,7 +30,6 @@ public class GDXGraphics implements Graphics {
     boolean alphaEnabled = false;
     boolean blendMode = false;
 
-    private float fontSize = 16;
     private float lineWidth = 0;
     private int alpha = 0xff;
     private Camera orthographicCamera;
@@ -112,11 +112,15 @@ public class GDXGraphics implements Graphics {
     }
 
     @Override
-    public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+    public void drawArc(int x, int y, int w, int h, int startAngle, int arcAngle) {
+        int radius = w / 2 + 1;
+        int cx = x + radius;
+        int cy = height - y - h / 2 - 1;
+
         endBatch();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(batchColor);
-        shapeRenderer.arc(x, y, width, startAngle, arcAngle);
+        arc(cx, cy, radius, startAngle, arcAngle, ShapeRenderer.ShapeType.Line);
         shapeRenderer.end();
     }
 
@@ -131,7 +135,6 @@ public class GDXGraphics implements Graphics {
         shapeRenderer.rect(x, height - y - h, w, h);
         shapeRenderer.end();
     }
-
 
     public void drawOval(int x, int y, int w, int h) {
         endBatch();
@@ -150,11 +153,15 @@ public class GDXGraphics implements Graphics {
     }
 
     @Override
-    public void fillArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+    public void fillArc(int x, int y, int w, int h, int startAngle, int arcAngle) {
+        int radius = w / 2 + 1;
+        int cx = x + radius;
+        int cy = height - y - h / 2 - 1;
+
         endBatch();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(batchColor);
-        shapeRenderer.arc(x, y, width, startAngle, arcAngle);
+        arc(cx, cy, radius, startAngle, arcAngle, ShapeRenderer.ShapeType.Filled);
         shapeRenderer.end();
     }
 
@@ -344,6 +351,54 @@ public class GDXGraphics implements Graphics {
 
     public void flush() {
         endBatch();
+    }
+
+    // Extracted from ShapeRenderer
+    private void arc(float x, float y, float radius, float start, float degrees, ShapeRenderer.ShapeType shapeType) {
+        arc(x, y, radius, start, degrees, Math.max(1, (int) (6 * (float) Math.cbrt(radius) * (degrees / 360.0f))), shapeType);
+    }
+
+    private void arc(float x, float y, float radius, float start, float degrees, int segments, ShapeRenderer.ShapeType shapeType) {
+        if (segments <= 0) throw new IllegalArgumentException("segments must be > 0.");
+        float colorBits = shapeRenderer.getColor().toFloatBits();
+        float theta = (2 * MathUtils.PI * (degrees / 360.0f)) / segments;
+        float cos = MathUtils.cos(theta);
+        float sin = MathUtils.sin(theta);
+        float cx = radius * MathUtils.cos(start * MathUtils.degreesToRadians);
+        float cy = radius * MathUtils.sin(start * MathUtils.degreesToRadians);
+
+        if (shapeType == ShapeRenderer.ShapeType.Line) {
+            for (int i = 0; i < segments; i++) {
+                shapeRenderer.getRenderer().color(colorBits);
+                shapeRenderer.getRenderer().vertex(x + cx, y + cy, 0);
+                float temp = cx;
+                cx = cos * cx - sin * cy;
+                cy = sin * temp + cos * cy;
+                shapeRenderer.getRenderer().color(colorBits);
+                shapeRenderer.getRenderer().vertex(x + cx, y + cy, 0);
+            }
+        } else {
+            for (int i = 0; i < segments; i++) {
+                shapeRenderer.getRenderer().color(colorBits);
+                shapeRenderer.getRenderer().vertex(x, y, 0);
+                shapeRenderer.getRenderer().color(colorBits);
+                shapeRenderer.getRenderer().vertex(x + cx, y + cy, 0);
+                float temp = cx;
+                cx = cos * cx - sin * cy;
+                cy = sin * temp + cos * cy;
+                shapeRenderer.getRenderer().color(colorBits);
+                shapeRenderer.getRenderer().vertex(x + cx, y + cy, 0);
+            }
+            shapeRenderer.getRenderer().color(colorBits);
+            shapeRenderer.getRenderer().vertex(x, y, 0);
+            shapeRenderer.getRenderer().color(colorBits);
+            shapeRenderer.getRenderer().vertex(x + cx, y + cy, 0);
+        }
+
+        cx = 0;
+        cy = 0;
+        shapeRenderer.getRenderer().color(colorBits);
+        shapeRenderer.getRenderer().vertex(x + cx, y + cy, 0);
     }
 
 }
